2fa96f8a2225d6381a2868ab8a03d40b9a5d860b
Üllar Seerme <yates@protonmail.ch>
2018-10-12 22:25:16 +0300

    Add functionality to either download specific release of Directus or latest release, also additional stylistic changes

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,28 +1,39 @@
 #!/usr/bin/env bash
 
 # Directus installation path
-DIR=/var/www/html
+DIR='/var/www/html'
 PROJECT_NAME='directus'
 PROJECT_DIR="$DIR/$PROJECT_NAME"
+# Leave empty for latest release or use specific
+# version number for release (e.g. '6.4.9')
+PROJECT_VERS=''
 
-# make directus directory
-sudo mkdir $PROJECT_DIR
+mkdir "$PROJECT_DIR"
 
-get_directus(){
-  if [ ! -d "$PROJECT_DIR/.git" ]; then
-    # can't git clone on a non-empty directory
-    git clone https://github.com/directus/directus.git $PROJECT_DIR
-    pushd $PROJECT_DIR
+get_directus() {
+  if [ ! -d "${PROJECT_DIR}/.git" ]; then
+    DOWNLOAD_CMD="wget -O ${PROJECT_NAME}.tar.gz https://api.github.com/repos/${PROJECT_NAME}/${PROJECT_NAME}/tarball"
+
+    if [ -n "$PROJECT_VERS" ]; then
+      DOWNLOAD_CMD="${DOWNLOAD_CMD}/${PROJECT_VERS}"
+    fi
+
+    # Don't double quote as it needs to execute the command stored within
+    $DOWNLOAD_CMD
+
+    tar -xf "${PROJECT_NAME}.tar.gz" -C "$PROJECT_DIR" --strip 1
+
+    pushd "$PROJECT_DIR"
       composer install
     popd
   fi
 }
 
 install_directus() {
-  DIRECTUS_CLI=$PROJECT_DIR/bin/directus
-  php $DIRECTUS_CLI install:config -h localhost -u root -p 123 -n directus
-  php $DIRECTUS_CLI install:database
-  php $DIRECTUS_CLI install:install -e admin@getdirectus.com -p password -t "Directus Demo"
+  DIRECTUS_CLI="${PROJECT_DIR}/bin/directus"
+  php "$DIRECTUS_CLI" install:config -h localhost -u root -p 123 -n directus
+  php "$DIRECTUS_CLI" install:database
+  php "$DIRECTUS_CLI" install:install -e admin@getdirectus.com -p password -t "Directus Demo"
 }
 
 apt-get update
